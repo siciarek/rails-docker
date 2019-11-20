2ec615b6b3dbd4dd917e734e5881f9cfd41d6fee
Jacek Siciarek <jacek.siciarek@nokaut.pl>
2019-11-20 09:21:12 +0100

    Update makefile

--- Makefile.mongo.mk
+++ Makefile.mongo.mk
@@ -10,12 +10,16 @@ pre:
 
 build:
 	$(MAKE) pre
-	docker-compose run app rails new . --api --force --no-deps --database=mysql
+	docker-compose run app rails new . --api --skip-active-record --force --no-deps --database=mysql
+	echo "gem 'mongoid', '~> 6.0'" >> Gemfile
+	echo "gem 'bson_ext'" >> Gemfile
 	sudo chown -R ${USER}:$(shell id -gn ${USER}) .
 	mv database.yml config/
 	docker-compose up --build --remove-orphans -d
-	docker-compose run app rake db:create
+	docker-compose run app bin/rails db:create
 	docker-compose down
+	docker-compose run app bin/rails g mongoid:config
+	docker-compose run app bin/rails g scaffold article name:string content:text
 	@echo 'YOUR PROJECT TEMPLATE HAS BEEN CREATED SUCCESSFULLY.'
 
 reset:
